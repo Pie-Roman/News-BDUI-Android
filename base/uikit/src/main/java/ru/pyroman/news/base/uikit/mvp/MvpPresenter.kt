630a385466c2da8eec1d17c58a43fe9f0cab612b
DoomPI <rslomtev@edu.hse.ru>
2024-05-16 21:46:05 +0300

    добавил base модуль

---
+++ base/uikit/src/main/java/ru/pyroman/news/base/uikit/mvp/MvpPresenter.kt
@@ -0,0 +1,29 @@
+package ru.pyroman.news.base.uikit.mvp
+
+import java.lang.ref.WeakReference
+
+open class MvpPresenter<View : MvpView> {
+
+    protected val view: View?
+        get() = _view?.get()
+
+    private var _view: WeakReference<View>? = null
+    private var isFirstLaunch = true
+
+    protected open fun onFirstViewAttach() {
+
+    }
+
+    open fun attachView(view: View) {
+        this._view = WeakReference(view)
+
+        if (isFirstLaunch) {
+            isFirstLaunch = false
+            onFirstViewAttach()
+        }
+    }
+
+    open fun detachView() {
+        this._view?.clear()
+    }
+}
